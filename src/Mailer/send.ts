f39c3aac80c4aedd5ece520c438c7ad893c2feef
bischmlb <mathiasbischo@gmail.com>
2024-08-24 15:32:01 +0200

    use npx

--- src/Mailer/send.ts
+++ src/Mailer/send.ts
@@ -7,7 +7,7 @@ fs.readFile(`${PATH_TO_DATA}/AddUserToWorkshop_0.json`, 'utf-8', async (_, data)
     const dataParsed: DataFile = JSON.parse(data);
     const subject = `(${dataParsed.title}): ${dataParsed.description}`;
     try {
-        await Mailer.send({ to: 'mathiasbischo@gmail.com', subject, html: dataParsed.html });
+        await Mailer.send({ to: '', subject, html: dataParsed.html });
         console.log('...success');
     } catch (err) {
         console.error(err);
